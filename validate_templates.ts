34f78acbba6a328a0f6a470ba788e0baa4e356b6
Elaina Lee <hyehwalee@microsoft.com>
2024-07-24 16:42:27 -0700

    added strictly enforced validation test with updated manifests following tests

--- validate_templates.ts
+++ validate_templates.ts
@@ -1,15 +1,18 @@
 import { program } from 'commander';
 import { z } from "zod";
-import { readFileSync } from 'fs';
+import { readFileSync, readdirSync } from 'fs';
 import path from 'path';
 import { fromError } from 'zod-validation-error';
+
 const manifestSchema = z.object({
     title: z.string(),
     description: z.string(),
     prerequisites: z.string().optional(),
     skus: z.array(z.union([z.literal('standard'), z.literal('consumption')])),
     details: z.object({
-        By: z.string(),
+        By: z.string().regex(/^[A-Z].*$/, {
+            message: 'By field must start with the first letter capitalized'
+        }),
         Type: z.union([z.literal('Workflow'), z.literal('Other')]),
         Trigger: z.union([z.literal('Request'), z.literal('Recurrence'), z.literal('Event')]),
         Category: z.string().optional()
@@ -17,12 +20,22 @@ const manifestSchema = z.object({
     detailsDescription: z.string().optional(),
     tags: z.array(z.string()).optional(),
     kinds: z.array(z.union([z.literal('stateful'), z.literal('stateless')])),
-    artifacts: z.array(z.object({ type: z.string(), file: z.string() })),
-    images: z.object({ light: z.string(), dark: z.string() }),
+    artifacts: z.array(z.object({ 
+        type: z.union([z.literal('workflow'), z.literal('map'), z.literal('schema'), z.literal('assembly')]),
+        file: z.string().regex(/^\S+\.\S+$/, {
+            message: 'File field must not contain spaces and must have an extension'
+        })
+    })),
+    images: z.object({ 
+        light: z.string(),
+        dark: z.string()
+    }),
     parameters: z.array(
         z.object({
             name: z.string().regex(/^\S*$/),
-            displayName: z.string(),
+            displayName: z.string().regex(/^[A-Z].*$/, {
+                message: 'Parameters "displayName" field must start with the first letter capitalized'
+            }),
             type: z.union([z.literal('String'), z.literal('Bool'), z.literal('Array'), z.literal('Float'), z.literal('Int'), z.literal('Object')]),
             description: z.string(),
             required: z.boolean(),
@@ -32,32 +45,76 @@ const manifestSchema = z.object({
         })
     ),
     connections: z.record(z.string(), z.object({
-        connectorId: z.string(),
-        kind: z.string()
+        connectorId: z.string().regex(/^\/.*/, {
+            message: 'Connections "connectorId" field must start with a forward slash'
+        }),
+        kind: z.union([z.literal('inapp'), z.literal('shared'), z.literal('custom')]),
     }))
 })
 
 program.parse();
 
 
-
-const manifestList = JSON.parse(readFileSync(path.resolve('./manifest.json'), {
+const manifestNamesList = JSON.parse(readFileSync(path.resolve('./manifest.json'), {
     encoding: 'utf-8'
 }));
-for (let folder of manifestList) {
-    const manifestFile = JSON.parse(readFileSync(path.resolve(`./${folder}/manifest.json`), {
+
+const checkFilesExistCaseSensitive = (fileNamesInFolder: string[], folderName: string, listedFileNames: string[]) => {
+    for (const fileName of listedFileNames) {
+        if (!fileNamesInFolder.includes(fileName)) {
+            console.error(`Template Failed Validation: ${`./${folderName}/${fileName}`} not found`);
+            throw '';
+        }
+    }
+}
+
+const allowedCategories = ["Design Patterns", "Generative AI", "B2B", "EDI", "Approval", "RAG", "Automation", "BizTalk Migration", "Mainframe Modernization"];
+
+// Check all folders listed in manifest.json exist (case sensitive check)
+const allFileNamesInMainFolder = readdirSync(path.resolve("./"));
+checkFilesExistCaseSensitive(allFileNamesInMainFolder, '', manifestNamesList);
+
+for (const folderName of manifestNamesList) {
+    const manifestFile = JSON.parse(readFileSync(path.resolve(`./${folderName}/manifest.json`), {
         encoding: 'utf-8'
     }));
     const result = manifestSchema.safeParse(manifestFile);
 
-
     if (!result.success) {
-        console.log(`Template Failed Validation: ${folder}`)
+        console.log(`Template "${folderName}" Failed Validation`);
         const validationError = fromError(result.error);
         console.error(validationError.toString());
         throw '';
     }
 
+    const invalidLinkPatternMD = z.string().regex(/^.*\[\S+\]\s+\(\S+\).*$/, {
+        message: "String must match the pattern: anything [anything] (anything) anything",
+      });
+      const prerequisitesInvalidPattern = invalidLinkPatternMD.safeParse(manifestFile?.prerequisites ?? "");
+      const detailsDescriptionInvalidPattern = invalidLinkPatternMD.safeParse(manifestFile?.detailsDescription ?? "");
+      
+      if (prerequisitesInvalidPattern.success) {
+        console.error(`Template "${folderName}" Failed Validation: prerequisites link is invalid, ensure no space between the [text] and the (link)`);
+        throw '';
+      }
+      if (detailsDescriptionInvalidPattern.success) {
+        console.error(`Template "${folderName}" Failed Validation: detailsDescription link is invalid, ensure no space between the [text] and the (link)`);
+        throw '';
+      }
+
+    if (manifestFile.details?.Category) {
+        for (const category of manifestFile.details?.Category?.split(",") ?? []) {
+            if (!allowedCategories.includes(category)) {
+                console.error(`Template "${folderName}" Failed Validation: Category "${category}" is invalid`);
+                throw '';
+            }
+        }
+    }
+
+    // Check all artifacts/images listed in manifest.json exist (case sensitive check)
+    const fileNamesInFolder = readdirSync(path.resolve(`./${folderName}`));
+    checkFilesExistCaseSensitive(fileNamesInFolder, folderName, manifestFile.artifacts.map((artifact) => artifact.file));
+    checkFilesExistCaseSensitive(fileNamesInFolder, folderName, [`${manifestFile.images.light}.png`, `${manifestFile.images.dark}.png`]);
 }
 
-console.log("Test Passed")
+console.log("Test Passed");
