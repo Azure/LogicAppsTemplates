1dd00c65365800b49905547a4a996e025f3dcb6e
Elaina Lee <144840522+Elaina-Lee@users.noreply.github.com>
2025-02-19 13:42:42 -0800

    Connection validation for consumption-only templates (#68)
    * added validation
    
    * added logic for replacing connection with suffix in workflow json for just workflow json missing suffix
    
    * added check to ensure it is consumption only for @parameters('connections...')

--- validate_templates.ts
+++ validate_templates.ts
@@ -173,18 +173,17 @@ const validateManifest = (folderName: string, isMultiWorkflow, manifestFile) => 
         //     throw '';
         // }
 
-        // const parameterNames =  manifestFile.parameters.map(parameter => parameter.name);
+        const parameterNames =  manifestFile.parameters.map(parameter => parameter.name);
         const connectionNames = Object.keys(manifestFile.connections);
     
-        // const parameterMatches = workflowFileString.matchAll(/@parameters\('\s*([^"]+)\s*'\)/g);
-        
-        // for (const match of parameterMatches) {
-        //     if (!parameterNames.includes(match[1])) {
-        //         console.error(`Workflow "${folderName}" Failed Validation: parameter "${match[1]}" not found in manifest.json. Hint: Make sure the parameter name is in the format <parameterName>_#workflowname#`);
-        //         throw '';
-        //     }
-        // }
-     
+        const parameterMatches = workflowFileString.matchAll(/@parameters\('\s*(?!\$connections)([^"]+)\s*'\)/g);
+        for (const match of parameterMatches) {
+            if (!parameterNames.includes(match[1])) {
+                console.error(`Workflow "${folderName}" Failed Validation: parameter "${match[1]}" not found in manifest.json. Hint: Make sure the parameter name is in the format <parameterName>_#workflowname#`);
+                throw '';
+            }
+        }
+
         const connectionReferenceMatches = workflowFileString.matchAll(/"connection":\s*\{\s*"referenceName":\s*"([^"]+)"\}/g);
         for (const match of connectionReferenceMatches) {
             if (!connectionNames.includes(match[1])) {
@@ -197,6 +196,21 @@ const validateManifest = (folderName: string, isMultiWorkflow, manifestFile) => 
         for (const match of connectionNameMatches) {
             if (!connectionNames.includes(match[1])) {
                 console.error(`Workflow "${folderName}" Failed Validation: connection used in "connectionName": "${match[1]}" not found in manifest.json. Hint: Make sure the connection name is in the format <connectionName>_#workflowname#`);
+                throw '';
+            }
+        }
+
+        const parameterConnectionsMatches = [...workflowFileString.matchAll(/@parameters\('\$connections'\)\['([^']+)'\]\['connectionId'\]/g)];
+
+        // If skus is not defined, it supports both
+        if (parameterConnectionsMatches?.length && (isMultiWorkflow || (manifestFile?.skus?.includes("standard") ?? true))) {
+            console.error(`Workflow "${folderName}" Failed Validation: @parameters('$connections') is invalid for standard workflows. Either remove the @parameters('$connections') or set the sku to "consumption" in manifest.json`);
+            throw '';
+        }
+
+        for (const match of parameterConnectionsMatches) {
+            if (!connectionNames.includes(match[1])) {
+                console.error(`Workflow "${folderName}" Failed Validation: @parameters('$connections') "${match[1]}" not found in manifest.json. Hint: Make sure the connection name is in the format <connectionName>_#workflowname#`);
                 throw '';
             }
         }
